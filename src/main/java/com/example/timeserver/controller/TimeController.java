b2bf9391f53ec7c24f9a540e7d2236dc134682b7
sgordon15 <kapster05@gmail.com>
2020-10-31 21:22:31 -0500

    hw1 wrote methods

--- src/main/java/com/example/timeserver/controller/TimeController.java
+++ src/main/java/com/example/timeserver/controller/TimeController.java
@@ -4,6 +4,8 @@ import com.example.timeserver.service.TimeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.*;
+import java.util.TimeZone;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,12 +18,26 @@ public class TimeController {
     }
 
     @GetMapping("/time")
-    public String getTime() {
-        return timeService.getTime();
+
+    public String getTime() { //gets time in utc
+        Instant utcTime = Instant.now();
+        String finalTime = utcTime.toString();
+        System.out.println(finalTime);
+        return finalTime;
     }
+
 
     @GetMapping("/epochTime")
     public Long getEpochTime() {
         return timeService.getEpochTime();
+
+    }
+    public String getTimeZone() {
+
+        LocalDateTime timeInZone = LocalDateTime.now(TimeZone.getTimeZone("America/New_York").toZoneId());
+        Instant time = Instant.now();
+        System.out.println("Time in New York:" +timeInZone);
+        return timeInZone.toString();
+
     }
 }
